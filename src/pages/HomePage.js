4344562eb4ea9fc8a2ee2a52589027b40c1b16a8
Purva shrivastva <shpurva6@gmail.com>
2024-04-23 18:43:42 +0530

    Doctor List Fetched Successfully

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Layout from "../components/Layout";
+import { Row } from "antd";
 
 const HomePage = () => {
+  const [doctors, setDoctors] = useState([]);
+
   //login user data
   const getUserData = async () => {
     try {
-      const res = await axios.post(
-        "/api/v1/user/getUserData",
-        {},
+      const res = await axios.get(
+        "/api/v1/user/getAllDoctors",
+
         {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("token"),
           },
         }
       );
+      if (res.data.success) {
+        setDoctors(res.data.data);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -25,7 +31,10 @@ const HomePage = () => {
   }, []);
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1 className="text-center">Home Page</h1>
+      {/* <Row>
+
+      </Row> */}
     </Layout>
   );
 };
